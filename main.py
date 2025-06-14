b2758c4da3f706b634b3a9b7ffb5482f6aaf64f5
GitSiege7 <carljklug@icloud.com>
2025-06-14 18:49:40 -0400

    Upgraded functionality and added test project files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 from sys import argv
 
 def main():
@@ -9,15 +10,34 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    verbose = "--verbose" in argv
+
     if len(argv) < 2:
         raise Exception("Insufficient CL arguments")
     
-    contents = argv[1]
+    user_prompt = argv[1]
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=contents)
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
+    
+    print("Model response:", response.text)
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
 
 main()
