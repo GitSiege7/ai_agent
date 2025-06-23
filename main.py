a4d4d80185718cc0a2c504899fe7958883a0227f
GitSiege7 <carljklug@icloud.com>
2025-06-23 13:23:52 -0400

    added loop to allow for repeated function calls and agent functionality

--- main.py
+++ main.py
@@ -5,6 +5,88 @@ from google.genai import types
 from sys import argv
 from functions.call_function import call_function
 
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "dir": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Gets content of a file as a string, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path of the file to read, limited to files in working directory.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs a subprocess python file and returns the output.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path to the python code to execute, constrained to working directory.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes given content to a given file path, constrained to working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path to write the output to, constrained to working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file given by the file path.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
+    ]
+)
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -28,110 +110,45 @@ def main():
     generate_content(client, messages, verbose)
 
 
-def generate_content(client, messages, verbose):    
-    schema_get_files_info = types.FunctionDeclaration(
-        name="get_files_info",
-        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-        parameters=types.Schema(
-            type=types.Type.OBJECT,
-            properties={
-                "file_path": types.Schema(
-                    type=types.Type.STRING,
-                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-                ),
-            },
-        ),
-    )
-
-    schema_get_file_content = types.FunctionDeclaration(
-        name="get_file_content",
-        description="Gets content of a file as a string, constrained to the working directory.",
-        parameters=types.Schema(
-            type=types.Type.OBJECT,
-            properties={
-                "file_path": types.Schema(
-                    type=types.Type.STRING,
-                    description="The file path of the file to read, limited to files in working directory.",
-                ),
-            },
-        ),
-    )
-
-    schema_run_python_file = types.FunctionDeclaration(
-        name="run_python_file",
-        description="Runs a subprocess python file and returns the output.",
-        parameters=types.Schema(
-            type=types.Type.OBJECT,
-            properties={
-                "file_path": types.Schema(
-                    type=types.Type.STRING,
-                    description="The file path to the python code to execute, constrained to working directory.",
-                ),
-            },
-        ),
-    )
-
-    schema_write_file = types.FunctionDeclaration(
-        name="write_file",
-        description="Writes given content to a given file path, constrained to working directory.",
-        parameters=types.Schema(
-            type=types.Type.OBJECT,
-            properties={
-                "file_path": types.Schema(
-                    type=types.Type.STRING,
-                    description="The file path to write the output to, constrained to working directory.",
-                ),
-                "content": types.Schema(
-                    type=types.Type.STRING,
-                    description="The content to write to the file given by the file path.",
-                ),
-            },
-        ),
-    )
-
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-            schema_get_file_content,
-            schema_run_python_file,
-            schema_write_file
-        ]
-    )
-    
-    system_prompt = """
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
-
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
-    
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
         config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
     )
     
-    if response.function_calls:
-        for call in response.function_calls:
-            content = call_function(call, verbose)
+    i = 1
 
-            if content.parts[0].function_response.response:
-                if verbose:
-                    print(f"-> {content.parts[0].function_response.response}")
-            else:
-                raise Exception("No function response detected")
+    while i < 20:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+        
+        if response.function_calls:
+            for call in response.function_calls:
+                content = call_function(call, verbose)
 
-    print("Model response:", response.text)
+                messages.append(content)
 
-    if verbose:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
+                if content.parts[0].function_response.response:
+                    if verbose:
+                        print(f"-> {content.parts[0].function_response.response}")
+                else:
+                    raise Exception("No function response detected")
+        else:
+            print(f"Model response: {response.text}")
+            break
+
+        if verbose:
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+        i += 1
+
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+        )
 
 
 main()
